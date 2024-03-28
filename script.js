a92e7e8e430a78241a1939325248b01e63e44108
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-28 08:51:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//    = assignment operator
+//   == comparison operator (compare if values are equal)
+//  === strict equality operator (compare if values & datatype are equal)
+//   != inequality operator
+//  !== strict inequality operator
+
+// const PI = 3.14;
+
+// if(PI == "3.14"){
+//     console.log("That is PI");
+// }
+// else{
+//     console.log("That is NOT PI")
+// }
+
+// const PI = 3.14;
+
+// if(PI != "3.14"){
+//     console.log("That is NOT PI");
+// }
+// else{
+//     console.log("That is PI")
+// }
+
+const PI = 3.14;
+
+if(PI !== 3.14){
+    console.log("That is NOT PI");
+}
+else{
+    console.log("That is PI")
+}
